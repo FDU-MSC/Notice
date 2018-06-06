367a191793e0c3469b02670337a779ee1bc882ce
will131 <1474349390@qq.com>
2018-06-06 14:53:02 +0800

    rebuild:sorted by date

--- automake.py
+++ automake.py
@@ -5,12 +5,7 @@ with open("README.md","w",encoding="utf-8") as fin:
     fin.write("# Notice\n微软学生俱乐部的通知信息\n\n")
     for root , dirs, files in os.walk("."):
         if root == ".":
+            files = sorted(files)
             for file in files:
-                if file.endswith(".md"):
+                if file[4]==file[7]:
                     fin.write("- %s\n"%file)
-# - 12月2日俱乐部大会.md
-# - 研究院项目俱乐部参与情况.md
-# - 俱乐部核心成员.md
-# - 俱乐部活动积分规则.md
-# - 俱乐部积分统计.md
-# - MSC微软学生俱乐部9-10月Newsletter.jpg
